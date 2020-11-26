62e9acf7ecf3e82aae6d4dc6e66ef1b726438114
Faustine <faustine.jacquet@epitech.eu>
2020-11-26 18:03:08 +0100

    pokedexfront/

--- pokedexfront/pokedex/src/components/HelloWorld.vue
+++ pokedexfront/pokedex/src/components/HelloWorld.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="hello">
     <div>
-      <b-dropdown text="Choose a pokemon">
-        <b-dropdown-item>{{}}</b-dropdown-item>
+      <b-dropdown variant="light" style="margin-top: 3vh; border: solid 1px" :text="type">
+        <b-dropdown-item @click="setType('type')">all</b-dropdown-item>
+        <b-dropdown-item v-for="type in types" :key="type" @click="setType(type)">{{type}}</b-dropdown-item>
       </b-dropdown>
     </div>
 
     <input type="string" name="search" class="searchbar" placeholder="search a pokemon" v-model="search">
+    <img src="../assets/ball.jpg" height="100vh" wigth="100vw" style="margin-bottom: 3vh; margin-left: 2vw;">
     <div class="container" style="margin-top: 5vh;">
       <div class="row">
         <div class="cardpokemons" v-for="(result, index) in getresults()" :key="result+index">
@@ -102,8 +104,9 @@
        results: [], 
        evolutions: [],
        search: '',
-       drop: [],
-       pokemon: null
+       pokemon: null,
+       types: [],
+       type: 'type'
      } 
    },
    mounted(){
@@ -119,6 +122,7 @@
       .then(response => {
         console.log(response)
         this.results=response
+        this.getType()
       })
     },
     getPoke(id){
@@ -160,14 +164,41 @@
       var filteredresults = [];
       for(var i=0; i<this.results.length; i++){
         let pokemon = this.results[i];
-        if (this.similar(pokemon.id, pokemon.name, pokemon.types)) {
+        if (this.similar(pokemon.id, pokemon.name, pokemon.types) && this.same(pokemon.types)) {
           filteredresults.push(pokemon);
         }
       }
       return filteredresults;
     },
+    same(types){
+      if(this.type==="type"){
+        return true;
+      }
 
+      let type1=types.type1.toLowerCase();
+      let type2=types.type2.toLowerCase();
 
+      if(type1==this.type || type2==this.type){
+        return true;
+      } 
+      return false;
+    },
+    setType(type){
+      this.type=type;
+    },
+    getType(){
+      this.types=[];
+      for(var i=0; i<this.results.length; i++){
+        var pokemon=this.results[i]
+        let type1=pokemon.types.type1.toString()
+        let type2=pokemon.types.type2.toString()
+        if (!this.types.includes(type1)) {
+          this.types.push(type1);
+        } if (!this.types.includes(type2) && type2!=="") {
+          this.types.push(type2);
+        }
+      } 
+    }
   }
 }
 </script>
